9d2b708d29ad8ed9b04e4132ddb0e53345c2aeb6
Alexandros1992 <alex.tsieris@hotmail.com>
2026-01-18 18:07:40 +0200

    Instatiating the second component.

---
+++ src/components/LiveClock.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect} from 'react';
+
+function LiveClock() {
+    const [time, setTime] = useState("");
+
+    useEffect(() => {
+        const updateTime = () => {
+            const formattedTime = new Intl.DateTimeFormat('en-GB', {
+                timeZone: timezone,
+                dateStyle: "full",
+                timeStyle: "long"
+            }).format(new Date());
+
+            setTime(formattedTime);
+        };
+
+        updateTime(); // Initial call to set time immediately
+        const interval = setInterval(updateTime, 1000); // Update every second
+
+        return () => clearInterval(interval); // Cleanup on unmount
+    }, [timezone]);
+
+    return (
+        <p><strong>Local Time:</strong>{time}</p>
+    );
+}
+
+export default LiveClock;
